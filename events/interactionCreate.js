262684521578ea026fd1be1af50896a327f4b7c6
Luen404 <dongdongdong0801@gmail.com>
2026-07-01 01:49:29 +0900

    test

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,27 +1,7 @@
 module.exports = (client) => {
+    console.log('EVENT LOADED'); // ← 이거 먼저 찍혀야 함
+
     client.on('interactionCreate', async (interaction) => {
-        if (!interaction.isChatInputCommand()) return;
-
-        const command = client.commands.get(interaction.commandName);
-        if (!command) {
-            console.log('커맨드 없음:', interaction.commandName);
-            return;
-        }
-
-        try {
-            await command.execute(interaction, client);
-        } catch (err) {
-            console.error(err);
-
-            if (!interaction.replied && !interaction.deferred) {
-                await interaction.reply({
-                    content: '에러 발생',
-                    ephemeral: true,
-                });
-            }
-        }
-        console.log('1 command:', interaction.commandName);
-console.log('2 found:', !!command);
-console.log('3 executed');
+        console.log('INTERACTION FIRED');
     });
 };

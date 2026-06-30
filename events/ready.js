1d723ff19cd0dcd1d6aabe428dcbee10a6ec9fef
luen404 <dongdongdong0801@gmail.com>
2026-07-01 03:03:31 +0900

    test11

--- events/ready.js
+++ events/ready.js
@@ -38,5 +38,34 @@ module.exports = {
         }
 
         await loadCommands(client);
+
+        const commandData = [];
+
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    
+    if ('data' in command && 'execute' in command) {
+        commandData.push(command.data.toJSON());
+    }
+}
+
+const rest = new REST({ version: '10' }).setToken(process.env.DSC_T);
+
+(async () => {
+    try {
+
+        const data = await rest.put(
+            Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+            { body: commandData }
+        );
+        console.log("성공")
+    } catch (error) {
+        console.log('실패', error)
+        }
+})();
     },
 };

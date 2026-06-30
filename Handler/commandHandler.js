f1a7770893f2ac0bbbc67091c2582b4727d5daa5
luen404 <dongdongdong0801@gmail.com>
2026-06-30 18:31:44 +0900

    1213

--- Handler/commandHandler.js
+++ Handler/commandHandler.js
@@ -9,25 +9,38 @@ module.exports = async (client) => {
 
     const commandsData = [];
 
-    for (const file of commandFiles) {
-        const filePath = path.join(commandsPath, file);
-        const command = require(filePath);
+    //~for (const file of commandFiles) {
+        //const filePath = path.join(commandsPath, file);
+        //const command = require(filePath);
 
-        if ('data' in command && 'execute' in command) {
-            client.commands.set(command.data.name, command);
-            commandsData.push(command.data.toJSON());
-        } else {
-            console.log(`${filePath}에 data || execute 미포함`);
-        }
+        //if ('data' in command && 'execute' in command) {
+            //client.commands.set(command.data.name, command);
+            //commandsData.push(command.data.toJSON());
+        //} else {
+            //console.log(`${filePath}에 data || execute 미포함`);
+        //}
+    //}
+    for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+
+    if (!command.data || !command.execute) continue;
+
+    const json = command.data.toJSON();
+
+    if (!json.name || !json.description) {
+        console.log('❌ 잘못된 커맨드:', file, json);
+        continue;
     }
+
+    console.log('✅ 정상:', json.name);
+
+    commandsData.push(json);
+}
 
     const rest = new REST({ version: '10' }).setToken(process.env.DSC_T);
 
     console.log('슬래시 커맨드 등록 시작');
-console.log('commandsData:', commandsData.length);
-console.log('client id:', process.env.CLIENT_ID);
-console.log('guild id:', process.env.GUILD_ID);
-
 try {
     const res = await rest.put(
         Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),

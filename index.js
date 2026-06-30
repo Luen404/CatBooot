b7dc5bf2ab03a5af88cc3e69e26c6031cf061679
luen404 <dongdongdong0801@gmail.com>
2026-07-01 04:01:36 +0900

    1jideq

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const client = new Client({
         GatewayIntentBits.Guilds,
         GatewayIntentBits.GuildMessages,
         GatewayIntentBits.MessageContents,
-        GatewayIntentBits.GuildVoiceStates,
+        GatewayIntentBits.GuildVoiceStates
     ]
 });
 
